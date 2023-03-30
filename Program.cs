dfe95c5ad121ca8c5ea4fa31ef223bd6f4b04e1e
Toleyorbek <128204542+Toleyorbek@users.noreply.github.com>
2023-03-30 23:23:09 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace min_max
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("4 ta son kitiring ");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            int d = int.Parse(Console.ReadLine());
+           int max=Math.Max(Math.Max(a, b),(Math.Max(c,d)));
+           int min=Math.Min(Math.Min(a, b),(Math.Min(c,d)));
+            Console.WriteLine(max-min);
+            
+        }
+    }
+}
