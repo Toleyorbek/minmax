ee38e27963cdb785b1d952ae3a3ad0600d90f30f
Toleyorbek <128204542+Toleyorbek@users.noreply.github.com>
2023-03-30 23:22:01 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("son kiriting ");
+            int a =int.Parse (Console.ReadLine());
+           for (int i = 0; i < a; i++) {
+            for (int j = 0; j < 10; j++)
+                {
+                    Console.WriteLine($"{ i } * { j }= {i * j } " );
+                }
+            }
+        }
+    }
+}
